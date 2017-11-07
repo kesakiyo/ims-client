79849676d03471914a9c8ffdae42a11a93197ea2
MinhoCheon <mino@MinhoCheonui-MacBook-Pro.local>
2017-11-07 23:02:43 +0900

    dev init

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/* External dependencies */
+import React from 'react'
+import ReactDom from 'react-dom'
+
+/* Render */
+ReactDom.render(
+  <div>hello</div>
+, window.document.getElementById('main'))
