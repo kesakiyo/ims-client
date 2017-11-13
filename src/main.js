2e718cf753da8883ed4403c4c2cfe789387ed336
MinhoCheon <mino@MinhoCheonui-MacBook-Pro.local>
2017-11-14 02:13:07 +0900

    Add timer for end time

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 /* External dependencies */
 import React from 'react';
+import moment from 'moment';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
+
+/* Set moment locale */
+moment.locale('ko');
 
 /* Internal dependencies */
 import routes from './routes';
